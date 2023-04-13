65d166d93bb45522d9858a0d29aa98afdcf7a0af
Melih Berkay Aydın <maydin13@ku.edu.tr>
2023-04-13 16:55:42 +0200

    Initial Commit for my personal site.

---
+++ src/app/lib/info.tsx
@@ -0,0 +1,20 @@
+import me from "../placeholder.jpg";
+
+export const name = "Melih Berkay Aydin";
+export const avatar = me;
+export const about = () => {
+  return (
+    <>
+      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as
+      a Software Engineer.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      When I'm not coding, I'm probably playing video games or watching anime. I
+      also love to travel and explore new places.
+    </>
+  );
+};
