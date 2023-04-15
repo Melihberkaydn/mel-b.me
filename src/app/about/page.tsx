ae18d69584d6f447464dfac983a68b74477ba019
Melih Berkay Aydın <maydin13@ku.edu.tr>
2023-04-15 16:58:23 +0200

    no

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,32 +5,7 @@ export default function AboutPage() {
     <Fragment>
       <h1 className="font-bold font-serif text-3xl">About me</h1>
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in nisl
-        elit. Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent
-        per conubia nostra, per inceptos himenaeos. Nullam elementum tincidunt
-        vulputate. Curabitur mollis elit non pretium aliquam. Phasellus porta
-        tortor vitae condimentum efficitur. Pellentesque mattis sem quis dolor
-        laoreet tristique. Vestibulum commodo leo volutpat erat sollicitudin
-        sagittis. Suspendisse tristique elit eget suscipit bibendum. Cras
-        convallis condimentum dictum. Curabitur varius eget quam eu molestie.
-        Fusce ac iaculis mauris, in sollicitudin ante. Duis pretium felis arcu,
-        id sagittis massa rutrum a. Praesent feugiat, orci nec hendrerit
-        blandit, lorem risus mollis quam, et aliquam mauris arcu eu sapien. Cras
-        laoreet congue justo, in mollis justo feugiat at. Vestibulum blandit
-        enim vel erat porta pharetra. Integer sit amet vulputate justo. In
-        turpis dui, dapibus nec risus vel, imperdiet maximus turpis. Fusce
-        venenatis, velit et venenatis condimentum, elit sem fermentum enim, quis
-        ornare neque nisi pharetra magna. Praesent non mauris hendrerit tellus
-        lobortis venenatis. Quisque a elementum est, ac accumsan sem. Mauris
-        gravida porta auctor. Integer quis ultricies tellus. Vestibulum eu
-        vulputate mi, nec egestas sapien. Fusce vel pellentesque nibh. Proin
-        eget sagittis leo, ac porttitor enim. In ut cursus tortor. Nam semper
-        condimentum ante. Duis erat enim, maximus vitae arcu a, dapibus
-        tristique erat. Aenean ultricies ante ac sapien bibendum rutrum.
-        Praesent maximus iaculis rhoncus. Quisque pretium erat et dui consequat
-        semper et sit amet lectus. Sed pharetra augue rhoncus diam feugiat, non
-        congue sapien aliquam.
+        Under Development
       </p>
     </Fragment>
   );
